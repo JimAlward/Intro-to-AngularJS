c0e8dd474525ae25106a2314849e8d52d3bd6f53
JimAlward <jimalward@hotmail.com>
2016-10-30 21:52:16 -0700

    added green/red color to text.

--- Assignment1/javaScript/app.js
+++ Assignment1/javaScript/app.js
@@ -6,13 +6,21 @@ angular.module('ListChecker', [])
 
 CheckerController.$inject = ['$scope'];
 function CheckerController($scope) {
+  $scope.textColor = "black";
+  $scope.borderSize = "5px";
+  $scope.borderType = "solid";
+  $scope.theBorderColor = "black";
   $scope.LunchList = "";
   $scope.LunchItems = [];
   $scope.numberOfItems = 0;
 
   $scope.checkLunchList = function () {
+    var emptyItems = false;
+    $scope.emptyItem = ""
     if ($scope.LunchList === "" || $scope.LunchList.trim().length == 0) {
       $scope.LunchListMessage = "Please enter data first!";
+      $scope.textColor = "red";
+      $scope.theBorderColor = "red";
     }
     else {
       var spliceIndexs = [];
@@ -33,15 +41,27 @@ function CheckerController($scope) {
         for (var i = spliceIndexs.length - 1; i >= 0; i--) {
           $scope.LunchItems.splice(spliceIndexs[i], 1);
           $scope.numberOfItems--;
+          emptyItems = true;
         }
       }
 
+      if (emptyItems) {
+        $scope.emptyItem = 'An empty item such as ", " is not included in the item count.';
+      }
+      else {
+        $scope.emptyItem = '';
+      }
       if ($scope.numberOfItems > 3) {
           $scope.LunchListMessage = "Too much!";
+          $scope.textColor = "green";
       } else if ($scope.numberOfItems == 0) {
           $scope.LunchListMessage = "Please enter data first!";
+          $scope.textColor = "red";
+          $scope.theBorderColor = "red";
       } else {
           $scope.LunchListMessage = "Enjoy!";
+          $scope.textColor= "green";
+          $scope.theBorderColor = "green";
       }
     }
   };
